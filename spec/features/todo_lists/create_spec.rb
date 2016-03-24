56996b16df22d76dea93d35b5c7d3bd2be4d990d
MarcoSantana <marco.santana@gmail.com>
2016-03-24 11:49:12 -0600

    Add length and presence validation to todo list Title and Description

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,65 @@
+#require 'spec_helper'
+require 'rails_helper'
+
+describe "when creating todo lists" do
+  it "should redirect to the todo list index page on success" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page). to have_content "New Todo List"
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: "Things to be done"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("Todo list was successfully created.")
+
+  end
+
+  it "should display an error when the todo list has no title" do
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page). to have_content "New Todo List"
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "Things to be done"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Things to be done")
+  end
+
+  it "should display an error when the todo list has no description" do
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page). to have_content "New Todo List"
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("My todo list")
+  end
+
+  it "should display an error when the title has less than 3 chars " do
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Hi" #Less than 3 chars length string
+    fill_in "Description", with: "Things to be done"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+  end
+end
